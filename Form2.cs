d099e45c3aba07af2a521176ac546e310ef3e683
lkuca <120181604+lkuca@users.noreply.github.com>
2023-11-16 10:00:19 +0200

    asdaasdfsfsfsf
    asfdasfafafafadfsdfsf

--- Form2.cs
+++ Form2.cs
@@ -14,18 +14,63 @@ namespace Image_redactor
     public partial class Form2 : Form
     {
         PictureBox pbs;
-        NumericUpDown numick;
-        HScrollBar hsCSroll;
-        public Form2() 
+        NumericUpDown numicRed,numicGreen,numicBlue;
+        HScrollBar Red,Greem,Blue;
+        Color colorResult;
+        
+        public Form2(Color color) 
         {
-            
-            hsCSroll = new HScrollBar();
-            hsCSroll.Minimum= 0;
-            hsCSroll.Maximum= 255;
-            hsCSroll.Tag = numick;
+
+            Red.Tag = numicRed;
+            Greem.Tag= numicGreen;
+            Blue.Tag= numicBlue;
+
+            numicRed.Tag = Red;
+            numicGreen.Tag = Greem;
+            numicBlue.Tag= Blue;
+
+            numicRed.Tag = Color.Red;
+            numicGreen.Tag = Color.Green;
+            numicBlue.Tag = Color.Blue;
 
         }
+        private void Redvaluechange(object sender, EventArgs e)
+        {
+            ScrollBar scrollBar= (ScrollBar)sender;
+            NumericUpDown numericUpDown1 = (NumericUpDown)sender;
+            numericUpDown1.Value = scrollBar.Value;
+        }
+        private void redvluechange(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown1 = (NumericUpDown)sender;
+            ScrollBar scrollBar= (ScrollBar)numericUpDown1.Tag;
+            scrollBar.Value = (int)numericUpDown1.Value;
+        }
+        private void Greemvaluechange(object sender, EventArgs e)
+        {
+            ScrollBar scrollBar = (ScrollBar)sender;
+            NumericUpDown numericUpDown1 = (NumericUpDown)sender;
+            numericUpDown1.Value = scrollBar.Value;
+        }
+        private void Greemvluechange(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown1 = (NumericUpDown)sender;
+            ScrollBar scrollBar = (ScrollBar)numericUpDown1.Tag;
+            scrollBar.Value = (int)numericUpDown1.Value;
+        }
+        private void Bluevaluechange(object sender, EventArgs e)
+        {
+            ScrollBar scrollBar = (ScrollBar)sender;
+            NumericUpDown numericUpDown1 = (NumericUpDown)sender;
+            numericUpDown1.Value = scrollBar.Value;
+        }
+        private void Bluevluechange(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown1 = (NumericUpDown)sender;
+            ScrollBar scrollBar = (ScrollBar)numericUpDown1.Tag;
+            scrollBar.Value = (int)numericUpDown1.Value;
+        }
+        private void UpdateColor()
 
-        
     }
 }
